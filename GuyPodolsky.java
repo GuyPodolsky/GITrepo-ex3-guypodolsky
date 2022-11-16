e5742f1e5369e576515298f937a774009cf3b8c9
Guy Podolsky <guypo@mta.ac.il>
2022-11-16 20:50:53 +0200

    second change in GuyPodolsky.java

--- GuyPodolsky.java
+++ GuyPodolsky.java
@@ -1,7 +1,7 @@
 public class GuyPodolsky {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Hello Guy Podolsky..." + (i+1));
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Hello Guy Podolsky..." + (i + 1));
         }
     }
 }
